0170161db1b410f618e4688c2ec65b1d86cc6fff
Blittyyy <97271580+Blittyyy@users.noreply.github.com>
2025-06-03 00:58:21 -0400

    Fix Authentication Flow

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,6 @@ import type React from "react"
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import { createServerComponentClient } from "@supabase/auth-helpers-nextjs"
-import { cookies } from "next/headers"
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -13,13 +11,11 @@ export const metadata: Metadata = {
   generator: 'v0.dev'
 }
 
-export default async function RootLayout({
+export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode
 }>) {
-  const supabase = createServerComponentClient({ cookies })
-  
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>{children}</body>
